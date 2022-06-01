0ac5ebfd424fd00a6631848270ec70d7e59689a7
Abdulsayed <mohammed321735@gmail.com>
2022-06-01 11:37:56 +0200

    Add project files.

---
+++ Indexing Seq/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Indexing_Seq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.btnrr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 597);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(416, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 539);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(416, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Delete by id";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 510);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(416, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Search by id";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 481);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(416, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Load Index";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 452);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(416, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Load File";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 124);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 298);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(180, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(338, 61);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Insert";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btnrr
+            // 
+            this.btnrr.Location = new System.Drawing.Point(12, 568);
+            this.btnrr.Name = "btnrr";
+            this.btnrr.Size = new System.Drawing.Size(416, 23);
+            this.btnrr.TabIndex = 0;
+            this.btnrr.Text = "Rewrite";
+            this.btnrr.UseVisualStyleBackColor = true;
+            this.btnrr.Click += new System.EventHandler(this.btnrr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 636);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnrr);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btnrr;
+    }
+}
+
---
+++ Indexing Seq/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Indexing_Seq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            write(0);
+        }
+        SortedDictionary<int, int> d = new SortedDictionary<int, int>();
+        FileStream fs;
+        StreamReader sr;
+        StreamWriter sw;
+        FileStream fs1;
+        StreamReader sr1;
+        StreamWriter sw1;
+        public void write(int x)//write test
+        {
+            d.Clear();
+            fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            sr = new StreamReader(fs);
+            string l;
+            int c = 0;
+            while((l=sr.ReadLine())!=null)
+            {
+                if (l[0] == '*')
+                {
+                    c += l.Length + 2; continue;
+                }
+                string[] arr = l.Split('|');
+                d.Add(int.Parse(arr[0]), c);
+                c += l.Length+2;
+            }
+            sr.Close();
+            fs.Close();
+            write();
+        }
+        public void write()//write index
+        {
+            fs1 = new FileStream("ind.txt", FileMode.Truncate, FileAccess.ReadWrite);
+            sw1 = new StreamWriter(fs1);
+            foreach(var i in d)
+            {
+                sw1.WriteLine(i.Key + "\t" + i.Value);
+                sw1.Flush();
+            }
+            sw1.Close();
+            fs1.Close();
+        }
+        private void button7_Click(object sender, EventArgs e)//INSERT
+        {
+            int[] arr = d.Keys.ToArray();
+            if (!bs(arr, int.Parse(textBox2.Text)))
+            {
+                fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                sw = new StreamWriter(fs);
+                fs.Seek(0, SeekOrigin.End);
+                int c = Convert.ToInt32(fs.Position);
+                d.Add(int.Parse(textBox2.Text), c);
+                sw.Write(textBox2.Text + "|" + textBox3.Text + "\r\n");
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+                write();
+            }
+            else
+            {
+                MessageBox.Show("Already Added!");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)//LOADFILE
+        {
+            fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            sr=new StreamReader(fs);
+            string l;
+            string f = "";
+            while((l=sr.ReadLine())!=null)
+            {
+                if (l[0]!='*')
+                    f += l + "\r\n";
+            }
+            textBox1.Text = f;
+            sr.Close();
+            fs.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)//LOADINDEX
+        {
+            write();
+            fs1 = new FileStream("ind.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            sr1 = new StreamReader(fs1);
+            string l,f="";
+            while ((l=sr1.ReadLine())!=null)
+            {
+                if (l[0] == '*') continue;
+                f += l+ "\r\n";
+            }
+            textBox1.Text = f;
+            sr1.Close();
+            fs1.Close();
+        }
+        public bool bs(int[] arr,int n)
+        {
+            int st = 0, en = arr.Length - 1;
+            int mid;
+            while(st<=en)
+            {
+                mid = (st + en) / 2;
+                if (arr[mid] == n) return true;
+                if (arr[mid] < n) st = mid + 1;
+                else if (arr[mid] > n) en = mid - 1;
+            }
+            return false;
+        }
+        private void button4_Click(object sender, EventArgs e)//Search By ID
+        {
+            int[] arr = d.Keys.ToArray();
+            if(bs(arr, int.Parse(textBox2.Text)))
+            {
+                MessageBox.Show("Found!");
+                int h = Convert.ToInt32(textBox2.Text);
+                int m = d[h];
+                textBox1.Text = "ID = " + h.ToString()+" at Position = "+ m.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Not Found!");
+            }
+        }
+        public void delind()
+        {
+            sw1 = new StreamWriter("ind.txt");
+            int[] arr = d.Keys.ToArray();
+            if (bs(arr, int.Parse(textBox2.Text)))
+            {
+                d.Remove(int.Parse(textBox2.Text));
+                foreach (var i in d)
+                {
+                    sw1.WriteLine(i.Key + "\t" + i.Value);
+                    sw1.Flush();
+                }
+                MessageBox.Show("Deleted");
+            }
+            else MessageBox.Show("Not Found!");
+            sw1.Close();
+        }
+        public void deltxt()
+        {
+            fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            sr = new StreamReader(fs);
+            sw = new StreamWriter(fs);
+            string l;
+            int c = 0;
+            while ((l=sr.ReadLine())!=null)
+            {
+                string[] arr = l.Split('|');
+                if (arr[0]==textBox2.Text)
+                {
+                    fs.Seek(c, SeekOrigin.Begin);
+                    sw.Write("*");
+                    sw.Flush();
+                    break;
+                }
+                c += l.Length + 2;
+            }
+            fs.Close();
+        }
+        private void button3_Click(object sender, EventArgs e)//Delete By ID
+        {
+            delind();
+            deltxt();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Close
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnrr_Click(object sender, EventArgs e)//Rewrite
+        {
+            write(1);
+        }
+    }
+}
